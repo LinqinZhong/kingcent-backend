d8f4abe8afd47013105ef28b34c7104aa1d25910
dust <1576397897@qq.com>
2023-08-15 11:45:27 +0800

    order删除

--- kingcent-campus-admin/src/main/java/com/kingcent/campus/admin/service/impl/AdminOrderService.java
+++ kingcent-campus-admin/src/main/java/com/kingcent/campus/admin/service/impl/AdminOrderService.java
@@ -10,9 +10,11 @@ import com.kingcent.campus.shop.entity.vo.order.OrderStoreVo;
 import com.kingcent.campus.shop.entity.vo.purchase.PurchaseConfirmVo;
 import com.kingcent.campus.shop.listener.OrderListener;
 import com.kingcent.campus.shop.mapper.OrderMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author zzy
@@ -20,13 +22,32 @@ import java.util.List;
 @Service
 public class AdminOrderService extends ServiceImpl<OrderMapper, OrderEntity> implements OrderService {
 
+    @Autowired
+    private OrderMapper orderMapper;
     @Override
     public Result<?> deleteOrder(Long id) {
+
         if (id == null){
-            return Result.fail("该用户不存在");
+            return Result.fail("id不能为空");
         }
         LambdaQueryWrapper<OrderEntity> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(OrderEntity::getId, id);
+        //queryWrapper.in(OrderEntity::getStatus, -1, -4, -5);
+        OrderEntity orderEntity = orderMapper.selectOne(queryWrapper);
+        if (Objects.isNull(orderEntity)){
+            return Result.fail("删除订单不存在");
+        }
+        if (orderEntity.getStatus().equals(1)){
+            return Result.fail("待配送，无法删除该订单");
+        }if (orderEntity.getStatus().equals(2)){
+            return Result.fail("配送中，无法删除该订单");
+        }if (orderEntity.getStatus().equals(3)){
+            return Result.fail("已收货，无法删除该订单");
+        }if (orderEntity.getStatus().equals(-2)){
+            return Result.fail("已发送退款，无法删除该订单");
+        }if (orderEntity.getStatus().equals(-3)){
+            return Result.fail("商家同意退款，无法删除该订单");
+        }
         remove(queryWrapper);
         return Result.success();
     }
